dd4ddec0349d44b33f39462858e6f66dd4dd07fb
Dave <greencross.am@gmail.com>
2017-07-03 17:22:02 +0400

    Add basic functionality to the contact book

--- hw_01/index.js
+++ hw_01/index.js
@@ -1,3 +1,5 @@
+let addressbook = [];
+
 class Contact {
     constructor(id, firstName, lastName, phone) {
         this.id = id;
@@ -5,4 +7,30 @@ class Contact {
         this.lastName = lastName;
         this.phone = phone;
     }
+
+    get getId() {
+        return this.id;
+    }
+
+    get getName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+
+    set setPhone(newPhone) {
+        this.phone = newPhone;
+    }
 }
+
+// testing
+//let human = new Contact(1234, 'John', 'Smith', '555-456-77-66');
+//for (let prop in human) {
+//    console.log(`${prop}: ${human[prop]}`);
+//}
+
+let addContact = (id, firstName, lastName, phone) => {
+    addressbook.push(new Contact(id, firstName, lastName, phone));
+}
+
+addContact(1234, 'John', 'Smith', '555-545-88-99');
+console.log(addressbook);
+console.log(addressbook[0]);
