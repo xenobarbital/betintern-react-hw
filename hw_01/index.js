ba2bb2f37a0cd13e51fb40c02822f1fea2ee43ff
Dave <greencross.am@gmail.com>
2017-07-03 18:56:29 +0400

    Add function for deleting contacts.

--- hw_01/index.js
+++ hw_01/index.js
@@ -21,16 +21,24 @@ class Contact {
     }
 }
 
-// testing
-//let human = new Contact(1234, 'John', 'Smith', '555-456-77-66');
-//for (let prop in human) {
-//    console.log(`${prop}: ${human[prop]}`);
-//}
-
+// adding contact
 let addContact = (id, firstName, lastName, phone) => {
     addressbook.push(new Contact(id, firstName, lastName, phone));
 }
 
+// deleting contact
+let deleteContact = id => {
+    let i = addressbook.findIndex(elem => elem.id === id);
+    addressbook.splice(i, 1);
+}
+
+// testing
 addContact(1234, 'John', 'Smith', '555-545-88-99');
+addContact(1548, 'Penguin', 'Fat', '555-987-87-66');
+addContact(9875, 'Cormorant', 'Stupid', '555-874-22-33');
+
 console.log(addressbook);
-console.log(addressbook[0]);
+
+deleteContact(1548);
+
+console.log(addressbook);
