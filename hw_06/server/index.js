2555c62030d1beca4f5cbd4e8da163bc08196777
xenobarbital <greencross.am@gmail.com>
2017-08-19 09:56:21 +0400

    repaired the server for hw_06

--- hw_06/server/index.js
+++ hw_06/server/index.js
@@ -3,59 +3,51 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 const Twitter = require('twitter');
 const client = new Twitter({
-    consumer_key: 'aZxtaIRGKz5J9UQOc1S8cyryu',
-    consumer_secret: 'USmIMM0Jl21HaYYp5uU20lSwARykObFw7kXG0ja2k9ckOWbVPx',
-    access_token_key: '88262279-1dUFhEiahIOoWKooKwEC9hHIqvITvKbvCl7AgaXff',
-    access_token_secret: 'r2jHubSOgDlsIR3CHPBLeiXzBpvSzgZsCWnsQeCX6XmZd'
+  consumer_key: 'BSvYeUXvnvDHtVPrYOc65agGj',
+  consumer_secret: 'WngRCFFYUuVaFyWDe7xfvKdWiUMtPJQePu41iqjAQoiTh8rkgD',
+  access_token_key: '890157332465844224-BEjodQlLUxICjySSEHoZxPibORoMhjt',
+  access_token_secret: 'jo249AD4c0io0n6d45s0AxntAleiHqm75doWfMzj9XTTp'
 });
 
 const Server = require('simple-websocket/server');
 const server = new Server({ port: 3070 }); // see `ws` docs for other options
 
 let socket;
+
+server.on('connection', s => {
+  socket = s;
+});
+
 let stream;
 
-server.on('connection', function (mySocket) {
-    //socket.on('data', function (data) {});
-    //socket.on('close', function () {});
-    //socket.on('error', function (err) {});
-    socket = mySocket;
-});
-
-//post method
-
 app.post('/', (req, res) => {
-    if (req.body.keyword) {
-        console.log(req.body.keyword);
+  try {
+    stream && stream.destroy();
+  } catch (e) {
+    throw e;
+  }
 
-        try {
-            stream && stream.destroy();
-        } catch (e) {
-            //log
-        }
+  if (req.body.keyword) {
+    stream = client.stream('statuses/filter', {
+      track: req.body.keyword
+    });
 
-        //turning on the socket interface
-        stream = client.stream('statuses/filter', {
-            track: req.body.keyword
-        });
+    stream.on('data', event => {
+      socket.send(JSON.stringify(
+        event
+      ));
+    });
 
-        stream.on('data', function(event) {
-            socket.send(JSON.stringify(event));
-            console.log(event && event.text);
-        });
-
-        stream.on('error', function(error) {
-            throw error;
-        });
-        //end of socket stuff
-    }
-
-    res.send(req.body.keyword);
+    stream.on('error', error => {
+      console.log(error);
+    })
+  }
 });
 
-app.listen(3071, () => console.log('REST on 3071 :\)'));
+app.listen(3071, () => console.log('Server started at port 3071.'));
